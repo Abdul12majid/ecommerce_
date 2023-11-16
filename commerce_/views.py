ea71f9cfb2ce8cc057d950467cb5e57e14700fcc
Abdul12majid <yisaabdulmajid@gmail.com>
2023-11-16 07:31:16 +0100

    auth set-up

--- commerce_/views.py
+++ commerce_/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
@@ -7,4 +9,24 @@ def home(request):
 	return render(request, 'commerce/home.html', {'products':products})
 
 def login_user(request):
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		user = User.objects.filter(username=username).exists()
+		print(user)
+		if user is True:
+
+			#if not user.check_password():
+			#	messages.success(request, ('wrong password'))
+			#	return redirect(request.META.get("HTTP_REFERER"))
+			
+			user_ = authenticate(request, username=username, password=password)
+			if user_ is not None:
+				login(request, user_)
+				return redirect('/')
+			else:
+				messages.success(request, ('invalid details'))
+				return redirect(request.META.get("HTTP_REFERER"))
+		else:
+			return redirect('register')
 	return render(request, 'commerce/login.html', {})
