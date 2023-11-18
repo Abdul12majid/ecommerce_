be0283822d4485cdbcea21b8ba75aedc8b29ee4e
Abdul12majid <yisaabdulmajid@gmail.com>
2023-11-18 07:02:16 +0100

    user registration

--- commerce_/urls.py
+++ commerce_/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login', views.login_user, name='login-user'),   
+    path('login', views.login_user, name='login-user'),
+    path('create_account', views.create_account, name='create-account'),
+    path('product/<int:pk>', views.product, name='product'),
    # path('checkout', views.checkout, name='checkout'),
     #path('update_item/', views.update_item, name='update_item'),
 ]
--- commerce_/views.py
+++ commerce_/views.py
@@ -30,3 +30,34 @@ def login_user(request):
 		else:
 			return redirect('register')
 	return render(request, 'commerce/login.html', {})
+
+
+def create_account(request):
+	if request.method == 'POST':
+		username = request.POST['username']
+		first_name = request.POST['first_name']
+		last_name = request.POST['last_name']
+		email = request.POST['email']
+		password1 = request.POST['password1']
+		password2 = request.POST['password2']
+		if password1==password2:
+			if User.objects.filter(username=username).exists():
+				a = User.objects.filter(username=username).first()
+				return render(request, 'commerce/create_account.html', {'first_name':first_name, 'username':username, 'last_name':last_name, 'email':email, 'a':a})
+			elif User.objects.filter(email=email).exists():
+				b = User.objects.filter(email=email).exists()
+				return render(request, 'commerce/create_account.html', {'first_name':first_name, 'username':username, 'last_name':last_name, 'email':email, 'b':b})
+			else:
+				user = User.objects.create_user(first_name=first_name, username=username, last_name=last_name, email=email, password=password2)
+				user.save()
+				login(request, user)
+				return redirect('/')
+		else:
+			c = password1==password2
+			return render(request, 'commerce/create_account.html', {'first_name':first_name, 'username':username, 'last_name':last_name, 'email':email, 'c':c})
+	return render(request, 'commerce/create_account.html', {})
+
+
+def product(request, pk):
+	item = Product.objects.get(id=pk)
+	return render(request, 'commerce/product.html', {"item":item})
